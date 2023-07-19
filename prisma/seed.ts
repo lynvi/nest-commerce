353d70c281f8730a8a85ddb384e1b0bcd9b50d39
Abdelkader <aboukinanee@gmail.com>
2023-07-19 23:39:29 +0100

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -59,41 +59,43 @@ async function main() {
             data: product?.assets?.map((item) => ({ url: item })),
           },
         },
-        productVariants: variants &&
-          variants.length > 0 && {
-            create: [
-              ...variants.map((variant) => ({
-                ref: Math.floor(10000000 + Math.random() * 90000000) + '',
-                name: variant?.name || product.name,
-                description: product.description,
-                slug: slugify(product.slug),
-                price: variant.price,
-                stockLevel: variant.stockLevel,
+        productVariants:
+          variants && variants.length > 0
+            ? {
+                create: [
+                  ...variants.map((variant) => ({
+                    ref: Math.floor(10000000 + Math.random() * 90000000) + '',
+                    name: variant?.name || product.name,
+                    description: product.description,
+                    slug: slugify(product.slug),
+                    price: variant.price,
+                    stockLevel: variant.stockLevel,
 
-                assets: variant.assets && {
-                  createMany: {
-                    data: variant?.assets?.map((item) => ({ url: item })),
-                  },
-                },
-                productOptions: variant?.productOptions && {
-                  connectOrCreate: [
-                    ...variant?.productOptions.map((item) => ({
-                      create: {
-                        name: item.name,
-                        value: item.value,
+                    assets: variant.assets && {
+                      createMany: {
+                        data: variant?.assets?.map((item) => ({ url: item })),
                       },
-                      where: {
-                        name_value: {
-                          name: item.name,
-                          value: item.value,
-                        },
-                      },
-                    })),
-                  ],
-                },
-              })),
-            ],
-          },
+                    },
+                    productOptions: variant?.productOptions && {
+                      connectOrCreate: [
+                        ...variant?.productOptions.map((item) => ({
+                          create: {
+                            name: item.name,
+                            value: item.value,
+                          },
+                          where: {
+                            name_value: {
+                              name: item.name,
+                              value: item.value,
+                            },
+                          },
+                        })),
+                      ],
+                    },
+                  })),
+                ],
+              }
+            : {},
       },
     });
 
