37c8c434a91ab8c590d3256ca9ca53a82f5e1334
Abdelkader <aboukinanee@gmail.com>
2023-08-09 15:59:05 +0100

    collection input

--- src/collections/dto/create-collection.input.ts
+++ src/collections/dto/create-collection.input.ts
@@ -1,9 +1,19 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateCollectionInput {
   @Field(() => String, { description: 'collection name' })
   name: string;
+
+  @Field(() => String, { description: 'collection parent id', nullable: true })
+  parentId: string;
+
+  @Field(() => Int, {
+    description: 'collection parent id',
+    nullable: true,
+    defaultValue: 0,
+  })
+  position: number;
 
   @Field(() => String, { description: 'collection desc', nullable: true })
   description: string;
