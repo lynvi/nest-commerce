83585696fe9a4a914fd97f2f346045297502c1e7
Abdelkader <aboukinanee@gmail.com>
2023-08-10 23:21:08 +0100

    create product option

--- src/product-variants/dto/update-product-variant.input.ts
+++ src/product-variants/dto/update-product-variant.input.ts
@@ -1,8 +1,10 @@
 import { CreateProductVariantInput } from './create-product-variant.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateProductVariantInput extends PartialType(CreateProductVariantInput) {
-  @Field(() => Int)
-  id: number;
+export class UpdateProductVariantInput extends PartialType(
+  CreateProductVariantInput,
+) {
+  @Field(() => ID)
+  id: string;
 }
